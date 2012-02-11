167dd577bfd82af84f7bcc4ae6c27735757d6af5
Ashwin Amit <aamit1@uci.edu>
2012-02-11 03:48:26 -0800

    Worked a bit more on script view. Ran into some problems changing the font on word - maybe we should just highlight it. If we try to change the font then removing a string gets executed at a random time (I hate threading & concurrency in swing -_-). For the data model I was thinking one controller pointing to the database for any file, with methods like public String getPage (int pageNumber) or something maybe like public Page getPage (int pageNumber) with a helper class page with fields String pageText, int sceneNumber) something like that.

--- src/view/ScriptEditPanel.java
+++ src/view/ScriptEditPanel.java
@@ -1,21 +1,35 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.io.File;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 import scratchpad.PicturePanel;
+import controller.LoadScript;
 
 public class ScriptEditPanel extends JPanel
 {
@@ -27,13 +41,15 @@ public class ScriptEditPanel extends JPanel
 	private JTextPane stageDirections;
 	private JPanel navigationPanel;
 	
+	private String textToDisplay;
+	
 	public ScriptEditPanel (JTabbedPane parent)
 	{
 		super();
 		System.out.println ("Loadin ScriptEditPanel");
 		this.parent = parent;
 		floorPlan = new PicturePanel();
-		scriptPane = new ScriptPane (parent);
+		scriptPane = new ScriptPane();
 		cuePanel = new JPanel ();
 		stageDirections = new JTextPane ();
 		navigationPanel = new JPanel ();
@@ -50,7 +66,7 @@ public class ScriptEditPanel extends JPanel
 		//navigationpanel
 		
 		setLayout (new BoxLayout (this, BoxLayout.X_AXIS));
-		
+		cuePanel.setLayout(null);
 		
 		JScrollPane scroller = new JScrollPane (scriptPane);
         scroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -82,11 +98,143 @@ public class ScriptEditPanel extends JPanel
         add(navigationPanel);
 	}
 	
+	
+	private class ScriptPane extends JTextPane 
+	{
+		private PopupMenuListener ml;
+		
+		public ScriptPane ()
+		{
+			super();
+			ml = new PopupMenuListener();
+			addPopupMenu();
+		}
+		
+		private void addPopupMenu ()
+		{
+			JMenuItem menuItem;
+			JPopupMenu popup = new JPopupMenu();
+	        menuItem = new JMenuItem("Tag word as prop");
+	        menuItem.addActionListener(ml);
+	        menuItem.setActionCommand("tag");
+	        popup.add(menuItem);
+	        menuItem = new JMenuItem("Add cue here");
+	        menuItem.setActionCommand("cue");
+	        menuItem.addActionListener(ml);
+	        popup.add(menuItem);
+	        menuItem = new JMenuItem("Insert blocking at this location");
+	        menuItem.setActionCommand("blocking");
+	        menuItem.addActionListener(ml);
+	        popup.add(menuItem);
+	        
+	        MouseListener popupListener = new PopupListener(popup);
+	        addMouseListener(popupListener);
+	        
+		}
+		
+		private class PopupMenuListener implements ActionListener
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String ac = arg0.getActionCommand();
+				if (ac.equals("tag"))
+				{
+					
+				}
+				else if (ac.equals("cue"))
+				{
+			        Rectangle caretView = null;
+			        try {
+						caretView = modelToView(getCaret().getDot());
+					} catch (BadLocationException e1) {
+						e1.printStackTrace();
+					}
+			        //Get description for cue
+			        String cueDescription = JOptionPane.showInputDialog (null, "Enter cue description",
+			        		JOptionPane.YES_NO_CANCEL_OPTION);
+			        JLabel label = new JLabel (cueDescription); //put it into label
+			        label.setBorder(BorderFactory.createLineBorder(Color.black));
+			        
+			        //Put the label in the correct spot
+			        Insets insets = cuePanel.getInsets();
+			        Dimension size = label.getPreferredSize();
+			        label.setBounds(25 + insets.left, (int) caretView.getY() + insets.top, size.width, size.height);
+			        
+			        cuePanel.add(label);
+			        cuePanel.repaint();
+			        //Remove text and replace it with blue version of itself
+			        String content = getText();
+			        int index = content.indexOf(getSelectedText(), 0);
+			        String removedText = getSelectedText();
+			        try {
+			        	getDocument().remove(0, 100);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						System.out.println ("Pirates");
+						e.printStackTrace();
+					}
+			        SimpleAttributeSet sa = new SimpleAttributeSet();
+			        StyleConstants.setFontFamily(sa, "Monospace"); 
+			        StyleConstants.setItalic(sa, true);
+			        StyleConstants.setForeground(sa, Color.blue);
+			        try {
+						getDocument().insertString(index, removedText, sa);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				else if (ac.equals("blocking"))
+				{
+					
+				}
+			}
+			
+		}
+		
+	    private class PopupListener extends MouseAdapter {
+	        JPopupMenu popup;
+
+	        PopupListener(JPopupMenu popupMenu) {
+	            popup = popupMenu;
+	        }
+
+	        public void mousePressed(MouseEvent e) {
+	            maybeShowPopup(e);
+	        }
+
+	        public void mouseReleased(MouseEvent e) {
+	            maybeShowPopup(e);
+	        }
+
+	        private void maybeShowPopup(MouseEvent e) {
+	            if (e.isPopupTrigger()) {
+	                popup.show(e.getComponent(),
+	                           e.getX(), e.getY());
+	            }
+	        }
+	    }
+
+	    public void displayPage (String text)
+	    {
+	    	
+	    }
+
+	}
+
+	
+	
 	public void loadScript (String fileName) throws FileNotFoundException
 	{
-		Scanner scanner = new Scanner (new File(fileName));
 		setName(fileName);
-		repaint();
+		textToDisplay = LoadScript.loadScript(fileName);
+		try {
+			scriptPane.getDocument().insertString(0, textToDisplay, new SimpleAttributeSet());
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -94,5 +242,6 @@ public class ScriptEditPanel extends JPanel
 	{
 		super.paintComponent(g);
 	}
+	
 	
 }
