cd66ee59d1ed1e074200ca79c89857802e93fd0a
Ashwin Amit <aamit1@uci.edu>
2012-02-11 00:12:17 -0800

    Committed scrathpad cod

---
+++ ScrathPad/src/PicturePanel.java
@@ -0,0 +1,80 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+import javax.swing.JPanel;
+
+
+public class PicturePanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private HashMap <Point, Queue<Point>> lines;
+	private Set <Point> xPoints;
+	
+	public PicturePanel ()
+	{
+		super();
+		this.setOpaque(true);
+		lines = new HashMap <Point, Queue<Point>> ();
+		xPoints = new HashSet <Point> ();
+	}
+	
+	public void addPoints (Point p1, Point p2)
+	{
+		System.out.println ("Adding points");
+		if (lines.get(p1) == null)
+		{
+			System.out.println ("Adding to empty pointer");
+			lines.put(p1, new LinkedList<Point> ());
+		}
+		lines.get(p1).add(p2);
+	}
+	
+	public void addX (Point p1)
+	{
+		xPoints.add(p1);
+	}
+	
+    @Override
+    public void paintComponent(Graphics graphic2d) {
+    	super.paintComponent(graphic2d);
+    	Graphics2D g2 = (Graphics2D) graphic2d;
+    	if (!lines.isEmpty())
+        {
+        	for (Point point: lines.keySet())
+        	{
+        		Iterator <Point> it = lines.get(point).iterator();
+        		Point prev = it.next();
+        		Point current = null;
+        		while (it.hasNext())
+        		{
+        			System.out.println ("IT HAS");
+        			current = it.next();
+        			g2.drawLine(prev.x, prev.y, current.x, current.y);
+        			prev = current;
+        		}
+        	}
+        }
+    	if (!xPoints.isEmpty())
+    	{
+    		Iterator <Point> it = xPoints.iterator();
+    		while (it.hasNext())
+    		{
+    			Point p = it.next();
+    			g2.fillOval(p.x, p.y, 5, 5);
+    		}
+    	}
+    }	
+
+	
+	
+}
---
+++ ScrathPad/src/TestLineNumbers.java
@@ -0,0 +1,93 @@
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.Document;
+
+/**
+ * A class illustrating running line number count on JTextPane. Nothing
+is painted on the pane itself,
+ * but a separate JPanel handles painting the line numbers.<br>
+ *
+ * @author Daniel Sjšblom<br>
+ * Created on Mar 3, 2004<br>
+ * Copyright (c) 2004<br>
+ * @version 1.0<br>
+ */
+public class TestLineNumbers extends JPanel
+{
+	// for this simple experiment, we keep the pane + scrollpane as members.
+	JTextPane pane;
+	JScrollPane scrollPane;
+
+	public TestLineNumbers()
+	{
+		super();
+		setMinimumSize(new Dimension(30, 30));
+		setPreferredSize(new Dimension(30, 30));
+		setMinimumSize(new Dimension(30, 30));
+		pane = new JTextPane() // we need to override paint so that the
+		{
+			public void paint(Graphics g)
+			{
+				super.paint(g);
+				TestLineNumbers.this.repaint();
+			}
+		};
+		scrollPane = new JScrollPane(pane);
+
+	}
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+
+		// We need to properly convert the points to match the viewport
+		// Read docs for viewport
+		int start = pane.viewToModel(scrollPane.getViewport().getViewPosition()); //
+		int end =
+		pane.viewToModel(
+				new Point(
+						scrollPane.getViewport().getViewPosition().x + pane.getWidth(),
+						scrollPane.getViewport().getViewPosition().y + pane.getHeight()));
+		// end pos in doc
+
+		// translate offsets to lines
+		Document doc = pane.getDocument();
+		int startline = doc.getDefaultRootElement().getElementIndex(start) ;
+		int endline = doc.getDefaultRootElement().getElementIndex(end);
+
+		int fontHeight = g.getFontMetrics(pane.getFont()).getHeight(); // font
+
+		for (int line = startline, y = 0; line <= endline; line++, y +=
+		fontHeight)
+		{
+			g.drawString(Integer.toString(line), 0, y);
+		}
+
+	}
+	
+	public JTextPane getTextPane ()
+	{
+		return pane;
+	}
+
+	// test main
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout());
+		final TestLineNumbers nr = new TestLineNumbers();
+		frame.getContentPane().add(nr, BorderLayout.WEST);
+		frame.getContentPane().add(nr.scrollPane, BorderLayout.CENTER);
+		frame.pack();
+		frame.setSize(new Dimension(400, 400));
+		frame.show();
+	}
+}
